bac23d9390f7cc727e6e5105d938b0c50436bd26
Black Byte <blackbyte9@gmx.net>
2025-09-20 09:08:48 +0200

    Add Login Button

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
       "next-env.d.ts",
       "src/generated/**",
       "prisma/**",
+      "shadcn/**",
     ],
   },
   {
---
+++ src/components/nav/login-button.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Button } from "@/shadcn/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export const LoginButton = () => {
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push("/auth/login");
+    };
+    return (
+        <span onClick={onClick} className="cursor-pointer">
+            <Button size="lg" variant="secondary">
+                Sign in
+            </Button>
+        </span>
+    );
+};
