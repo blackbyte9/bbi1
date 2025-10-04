0a3a8e95fcd84bd57cd5a3c342d835e06008d440
Black Byte <blackbyte9@gmx.net>
2025-10-04 12:46:12 +0200

    Fix login logout button

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,4 +1,6 @@
-import NextAuth, { User } from "next-auth";
+import NextAuth, { User, Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { z } from "zod";
 import Credentials from "next-auth/providers/credentials";
 
 export const BASE_PATH = "/api/auth";
@@ -12,6 +14,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials): Promise<User | null> {
+        if (!credentials) {
+          return null; // Return null if no credentials are provided
+        }
+        const parsedCredentials = z
+          .object({
+            username: z.string().min(4).max(128),
+            password: z.string().min(6).max(128)
+          }).safeParse(credentials);
+        if (!parsedCredentials.success) {
+          return null; // Return null if validation fails
+        }
+
         const user = { id: "1", name: "Klaas", email: "blackbyte9@gmx.net" };
         if (credentials?.username === "klaas" && credentials?.password === "arbeit") {
           return user;
@@ -23,4 +37,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   basePath: BASE_PATH,
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }: { session: Session; token: JWT; }) {
+      if (session?.user) {
+        session.user.id = token.sub ?? "";
+      }
+      return session;
+    }
+  }
 });
